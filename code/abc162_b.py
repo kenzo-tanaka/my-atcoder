8913ea9c0f75c9eb92cc5ffef1f2c13cdc1ad484
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-07-30 08:59:44 +0900

    code/abc162_b: create

---
+++ code/abc162_b.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+result = 0
+
+for i in range(n):
+    i += 1
+    
+    if (i % 3 == 0) and (i % 5 == 0):
+        next
+    elif (i % 3 == 0):
+        next
+    elif (i % 5 == 0):
+        next
+    else:
+        result += i
+
+print(result)
