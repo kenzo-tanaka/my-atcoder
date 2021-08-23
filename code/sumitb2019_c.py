2f45b41261cb0074f6bb6dbdf2f8041af15372bd
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2021-08-24 08:45:12 +0900

    code/sumitb2019_c: ac

--- code/sumitb2019_c.py
+++ code/sumitb2019_c.py
@@ -1,17 +1,23 @@
-import itertools
+# --解説--
+
+# @see https://youtu.be/vVOwtLuh60U?t=2811
+
+# 典型的なDP問題だが、楽な解法:
+# 買う個数を決め打ちする
+# 買う個数をnとすると
+# 作れる金額は 100n+(0~5n)
+# 100n <= x <= 100n + 5n
+# n <= x/100まで試せばOK
+
+# --解説--
 
 X = int(input())
-list = list(range(100, 106))
-
+mx_n = X // 100
 result = 0
 
-for i in range(6):
+for i in range(mx_n):
     i += 1
-    for pair in itertools.combinations(list, i):
-        if X % sum(pair) == 0:
-            result = 1
-            break
-    if result == 1:
-        break
+    if (100 * i <= X <= 100 * i + 5 * i):
+        result = 1
 
 print(result)
