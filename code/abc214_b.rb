dc69d635db2fc828fc7e092538562c5b92c7e062
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-25 19:22:49 +0900

    Fix abc214_b

--- code/abc214_b.rb
+++ code/abc214_b.rb
@@ -1,13 +1,11 @@
-s,t = gets.chomp.split('').map(&:to_i)
+s,t = gets.chomp.split(' ').map(&:to_i)
 
-# TODO: なぜs+1まで見る必要があるのかわかっていない
 count = 0
-for i in (0..(s+1))
-    for j in (0..(s+1))
-        for k in (0..(s+1))
+for i in (0..(s))
+    for j in (0..(s))
+        for k in (0..(s))
             if (i+j+k) <= s && (i*j*k) <= t
                 count +=1
-                print [i,j,k]
             end
         end
     end
