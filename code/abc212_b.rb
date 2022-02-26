9f1294745a457f073f306b3abe67e399e2f6c10f
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-26 11:00:38 +0900

    Fix abc212_b

--- code/abc212_b.rb
+++ code/abc212_b.rb
@@ -3,9 +3,19 @@ require 'minitest/autorun'
 def answer(x)
     nums = x.split('').map(&:to_i)
     return "Weak" if nums.uniq.length == 1
-    return "Weak" if nums[0] + 1 == nums[1] && nums[1] + 1 == nums[2] && nums[2] + 1 == nums[3]
+    return "Weak" if nums_seq_check(nums[0], nums[1]) && nums_seq_check(nums[1], nums[2]) && nums_seq_check(nums[2], nums[3])
 
     "Strong"
+end
+
+def nums_seq_check(x,y)
+    if x == 9 && y == 0
+        true
+    elsif x+1 == y
+        true
+    else
+        false
+    end
 end
 
 class SampleTest < Minitest::Test
@@ -16,6 +26,11 @@ class SampleTest < Minitest::Test
 
     def test_seq_weak
         x = "1234"
+        assert_equal answer(x), 'Weak'
+    end
+
+    def test_seq_weak_with_nine
+        x = "9012"
         assert_equal answer(x), 'Weak'
     end
     
