acdff7fc1287e11fc6136e6e6b29f95cff0ee395
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-26 12:28:49 +0900

    Fix abc213_c add test

--- code/abc213_c.rb
+++ code/abc213_c.rb
@@ -1,7 +1,14 @@
-h,w,n = gets.chomp.split(' ').map(&:to_i)
-ab = []
-n.times do
-   ab << gets.chomp.split(' ').map(&:to_i) 
+require 'minitest/autorun'
+
+def answer(h, w, n, abs)
+   print abs
+   [[2,1], [1,2]]
+end
+
+class SampleTest < Minitest::Test
+   def test_1
+      assert_equal answer(4,5,2, [[3,2], [2,5]]), [[2,1], [1,2]]
+   end
 end
 
 # a = Aだけを取得した配列
@@ -13,6 +20,3 @@ end
 # 1行目には何も数字がないことがわかる
 # 4行目も何も数字がないことがわかる
 # 1,3,4列目も数字がないことがわかる
-
-# 2 5
-# 3 2 
