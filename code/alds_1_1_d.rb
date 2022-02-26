172eadc6c2df6ab506ee93be1e216dacf195fb34
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-26 20:25:32 +0900

    Fix alds

--- code/alds_1_1_d.rb
+++ code/alds_1_1_d.rb
@@ -1,5 +1,19 @@
 def answer(n:, r:)
-    min = -200_000
-    # r.each do |x|
-    # end
+    min = r[0]
+    max = -200_000
+    (1..(n-1)).each do |n|
+        x = r[n]
+        max = [max, x - min].max
+        min = [min, x].min
+    end
+    max
+end
+
+def main
+    n = gets.chomp.to_i
+    r = []
+    n.times do
+        r << gets.chomp.to_i
+    end
+    print answer(n: n, r: r)
 end
