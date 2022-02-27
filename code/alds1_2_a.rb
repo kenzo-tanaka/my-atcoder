456589ae74f76c47498fe3f814b6044e2eb98e9e
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-27 13:40:06 +0900

    tweak

--- code/alds1_2_a.rb
+++ code/alds1_2_a.rb
@@ -1,3 +1,6 @@
 def answer(array)
     expected = [[1,2,3,4,5], 8]
+    # TODO: 変数iを配列の先頭indexから末尾indexまで移動する
+    # TODO: 変数jは(N-1)..(i+1)までを減少しながら移動する(これが終了したとき i++ する)
+    # TODO: array[j] < array[j-1] であれば、順番の入れ替えをする
 end
