a8ab67ba44d79c21abf4828275aacf77dc36dd9b
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-27 19:13:46 +0900

    tweak

--- request_atcoder.rb
+++ request_atcoder.rb
@@ -2,10 +2,10 @@ require 'date'
 require 'uri'
 require 'net/http'
 
-# uri = URI('https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user=kenzo1995&from_second=1645868486')
-# res = Net::HTTP.get_response(uri)
-# puts res.body
-
 yesterday = (Date.today - 1).to_time
 one_week_ago = (Date.today - 7).to_time
 one_month_ago = (Date.today - 30).to_time
+
+uri = URI("https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user=kenzo1995&from_second=#{yesterday.to_i}")
+res = Net::HTTP.get_response(uri)
+puts res.body
