c7c9013ab747ce5e26152f854079d8cd06e40cc5
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-27 19:37:55 +0900

    reindent

--- request_atcoder.rb
+++ request_atcoder.rb
@@ -2,16 +2,15 @@ require 'date'
 require 'uri'
 require 'net/http'
 
-
 class RequestAtcoder
-    def self.get_body
-        yesterday = Date.today - 1
-        one_week_ago = Date.today - 7
-        one_month_ago = Date.today - 30
+  def self.get_body
+    yesterday = Date.today - 1
+    one_week_ago = Date.today - 7
+    one_month_ago = Date.today - 30
 
-        unixtime = yesterday.to_time.to_i
-        uri = URI("https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user=kenzo1995&from_second=#{unixtime}")
-        res = Net::HTTP.get_response(uri)
-        res.body
-    end
+    unixtime = yesterday.to_time.to_i
+    uri = URI("https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user=kenzo1995&from_second=#{unixtime}")
+    res = Net::HTTP.get_response(uri)
+    res.body
+  end
 end
