e8f994b324ceed7603383fbd2f1e83a56c737a1a
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-27 19:28:26 +0900

    mock

--- request_atcoder.rb
+++ request_atcoder.rb
@@ -6,9 +6,11 @@ yesterday = Date.today - 1
 one_week_ago = Date.today - 7
 one_month_ago = Date.today - 30
 
-def get_body(date:)
-    unixtime = date.to_time.to_i
-    uri = URI("https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user=kenzo1995&from_second=#{unixtime}")
-    res = Net::HTTP.get_response(uri)
-    res.body
+class RequestAtcoder
+    def get_body(date:)
+        unixtime = date.to_time.to_i
+        uri = URI("https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user=kenzo1995&from_second=#{unixtime}")
+        res = Net::HTTP.get_response(uri)
+        res.body
+    end
 end
--- spec/request_atcoder_spec.rb
+++ spec/request_atcoder_spec.rb
@@ -1,6 +1,12 @@
-RSpec.describe 'AtcoderRequest' do
-    describe 'foo' do
-        it '' do
+require_relative '../request_atcoder'
+
+RSpec.describe 'RequestAtcoder' do
+    describe 'get_problems' do
+        before do
+            allow(RequestAtcoder).to receive(:get_body).and_return([]) 
+        end
+
+        it do
             expect(true).to eq true
         end
     end
