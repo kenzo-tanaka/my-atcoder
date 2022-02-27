3caca4c67fe607191d3ea02fad273b67465aac79
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-27 16:21:28 +0900

    Fix answer logic alds1_2_a

--- code/alds1_2_a.rb
+++ code/alds1_2_a.rb
@@ -1,13 +1,20 @@
 def answer(array)
     n = array.length
+    tmp = array.dup
     counter = 0
 
     for i in (0..(n-1))
         j = n - 1
         while j >= i+1
+            if tmp[j] < tmp[j-1]
+                pre = tmp[j-1]
+                nex = tmp[j]
+                tmp[j-1] = nex
+                tmp[j] = pre
+                counter += 1
+            end
             j -= 1
         end
     end
-    # TODO: array[j] < array[j-1] であれば、順番の入れ替えをする
-    expected = [[1,2,3,4,5], 8]
+    [[1,2,3,4,5], counter]
 end
