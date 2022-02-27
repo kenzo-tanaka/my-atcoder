54f53a46ecc5f3f84e169e1231a5e1906407ec07
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-27 19:58:10 +0900

    enhance

--- request_atcoder.rb
+++ request_atcoder.rb
@@ -4,16 +4,20 @@ require 'uri'
 require 'net/http'
 
 class RequestAtcoder
+  def self.get_problems(date:)
+    prev_day = date
+    next_day = date + 1
+    JSON.parse(get_body(date: prev_day)) - JSON.parse(get_body(date: next_day))
+  end
+
   def self.get_body(date:)
     unixtime = date.to_time.to_i
     uri = URI("https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user=kenzo1995&from_second=#{unixtime}")
     res = Net::HTTP.get_response(uri)
     res.body
   end
-
-  def self.get_problems
-    yesterday = Date.today - 1
-    today = Date.today
-    JSON.parse(get_body(date: yesterday)) - JSON.parse(get_body(date: today))
-  end
 end
+
+print RequestAtcoder.get_problems(date: Date.today - 1)
+print RequestAtcoder.get_problems(date: Date.today - 7)
+print RequestAtcoder.get_problems(date: Date.today - 30)
