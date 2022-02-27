c8e05d6c8282f3b3b229d1996328137fd4c7fa40
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-27 19:55:02 +0900

    enhance

--- request_atcoder.rb
+++ request_atcoder.rb
@@ -1,4 +1,5 @@
 require 'date'
+require 'json'
 require 'uri'
 require 'net/http'
 
@@ -11,6 +12,8 @@ class RequestAtcoder
   end
 
   def self.get_problems
-    [{"id"=>29647312, "epoch_second"=>1645839697, "problem_id"=>"abc212_a", "contest_id"=>"abc212", "user_id"=>"kenzo1995", "language"=>"Ruby (2.7.1)", "point"=>0.0, "length"=>168, "result"=>"WA", "execution_time"=>58}, {"id"=>29647333, "epoch_second"=>1645839752, "problem_id"=>"abc212_a", "contest_id"=>"abc212", "user_id"=>"kenzo1995", "language"=>"Ruby (2.7.1)", "point"=>100.0, "length"=>173, "result"=>"AC", "execution_time"=>60}, {"id"=>29647587, "epoch_second"=>1645840892, "problem_id"=>"abc212_b", "contest_id"=>"abc212", "user_id"=>"kenzo1995", "language"=>"Ruby (2.7.1)", "point"=>200.0, "length"=>419, "result"=>"AC", "execution_time"=>61}, {"id"=>29649518, "epoch_second"=>1645848178, "problem_id"=>"abc213_c", "contest_id"=>"abc213", "user_id"=>"kenzo1995", "language"=>"Ruby (2.7.1)", "point"=>0.0, "length"=>648, "result"=>"WA", "execution_time"=>505}, {"id"=>29653650, "epoch_second"=>1645860106, "problem_id"=>"abc213_c", "contest_id"=>"abc213", "user_id"=>"kenzo1995", "language"=>"Ruby (2.7.1)", "point"=>300.0, "length"=>653, "result"=>"AC", "execution_time"=>523}]
+    yesterday = Date.today - 1
+    today = Date.today
+    JSON.parse(get_body(date: yesterday)) - JSON.parse(get_body(date: today))
   end
 end
