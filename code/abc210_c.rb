8588cc5bbb26a5c8844ce2c2ea499bd3e95c9283
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-28 20:39:02 +0900

    tweak

--- code/abc210_c.rb
+++ code/abc210_c.rb
@@ -1,7 +1,17 @@
 def ans(n:, k:, c:)
   max = 1
   (n - k).times do |i|
-    max = [[c[i], c[i+1], c[i+2]].uniq.length, max].max
+    array = c[i..(k-1+i)]
+    max = [array.uniq.length, max].max
+    break if max == k
   end
   max
 end
+
+def main
+  n, k = gets.chomp.split(' ').map(&:to_i)
+  c = gets.chomp.split(' ').map(&:to_i)
+  print ans(n: n, k: k, c: c)
+end
+
+# main
--- code/test/abc210_c_test.rb
+++ code/test/abc210_c_test.rb
@@ -2,15 +2,21 @@ require 'minitest/autorun'
 require_relative '../abc210_c'
 
 class SampleTest < Minitest::Test
-    def test_1
-        expected = 3
-        actual = ans(n: 7, k: 3, c: [1,2,1,2,3,3,1])
-        assert_equal expected, actual
-    end
+  def test_1
+    expected = 3
+    actual = ans(n: 7, k: 3, c: [1,2,1,2,3,3,1])
+    assert_equal expected, actual
+  end
 
-    # def test_2
-    #     expected = true
-    #     actual = true
-    #     assert_equal expected, actual
-    # end
+  def test_2
+    expected = 1
+    actual = ans(n: 5, k: 5, c: [4,4,4,4,4])
+    assert_equal expected, actual
+  end
+
+  def test_3
+    expected = 4
+    actual = ans(n: 10, k: 6, c: %w[304621362 506696497 304621362 506696497 834022578 304621362 414720753 304621362 304621362 414720753])
+    assert_equal expected, actual
+  end
 end
