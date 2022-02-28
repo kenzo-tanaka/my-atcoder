975df418df5e24aac1718c17521b1c5cf3ad7afb
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-02-28 19:59:31 +0900

    ac abc211_b

--- tmp.rb
+++ tmp.rb
@@ -1,32 +1,27 @@
 require 'minitest/autorun'
 
-def ans(s:, t:)
-    counter = 0
-    for a in 0..s
-        for b in 0..(s-a)
-            for c in 0..(s-a-b)
-                counter += 1 if a+b+c <= s && a*b*c <= t
-            end
-        end
-    end
-    counter
+def ans(s:)
+  array = %w[H 2B 3B HR]
+  return "Yes" if array.all? { |x| s.include? x }
+
+  "No"
 end
 
 def main
-    s,t=gets.chomp.split(' ').map(&:to_i)
-    ans(s: s, t:t)
+  s = []
+  4.times { s << gets.chomp }
+  print ans(s: s)
 end
 
 class SampleTest < Minitest::Test
-    def test_1
-        expected = 4
-        actual = ans(s:1, t:0)
-        assert_equal expected, actual
-    end
-
-    def test_2
-        expected = 2471
-        actual = ans(s: 30, t:100)
-        assert_equal expected, actual
-    end
+  def test_1
+    expected = "Yes"
+    actual = ans(s: %w[3B HR 2B H])
+    assert_equal expected, actual
+  end
+  def test_2
+    expected = "No"
+    actual = ans(s: %w[2B 3B HR 3B])
+    assert_equal expected, actual
+  end
 end
