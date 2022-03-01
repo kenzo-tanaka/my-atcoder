65fc9d475cfbce147243e228add93fbe733c31de
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-01 21:25:03 +0900

    tweak

--- code/abc208_c.rb
+++ code/abc208_c.rb
@@ -1,10 +1,19 @@
 def ans(n:, k:, a:)
-  array = Array.new(a.length, default = 0)
+  result = Array.new(a.length, default = 0)
   while k >= n
-    array.each_with_index do |x, idx|
-      array[idx] += 1
+    result.each_with_index do |x, idx|
+      result[idx] += 1
+      k -= 1
     end
-    k -= 1
   end
-  array
+
+  unless k == 0
+    sort = a.sort
+    sort[0..(k-1)].each do |x|
+      idx = a.index(x)
+      result[idx] += 1
+    end
+  end
+
+  result
 end
--- code/test/abc208_c_test.rb
+++ code/test/abc208_c_test.rb
@@ -2,15 +2,15 @@ require 'minitest/autorun'
 require_relative '../abc208_c'
 
 class SampleTest < Minitest::Test
-    def test_1
-        expected = [4,3]
-        actual = ans(n: 2, k: 7, a: [1,8])
-        assert_equal expected, actual
-    end
+  def test_1
+    expected = [4,3]
+    actual = ans(n: 2, k: 7, a: [1,8])
+    assert_equal expected, actual
+  end
 
-    def test_2
-        expected = [3]
-        actual = ans(n: 1, k: 3, a: [33])
-        assert_equal expected, actual
-    end
+  def test_2
+    expected = [3]
+    actual = ans(n: 1, k: 3, a: [33])
+    assert_equal expected, actual
+  end
 end
