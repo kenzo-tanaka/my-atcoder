b185ead1205f5852c1ade8675e0ddaaee1676efe
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-01 20:37:07 +0900

    fix: uniq by contest_id, problem_id

--- request_atcoder.rb
+++ request_atcoder.rb
@@ -4,18 +4,19 @@ require 'uri'
 require 'net/http'
 
 class RequestAtcoder
-  def self.get_problems(date:)
-    prev_day = date
-    next_day = date + 1
-    result = JSON.parse(get_body(date: prev_day)) - JSON.parse(get_body(date: next_day))
-    result.select { |x| x["result"] = "WA" }
-  end
-
   def self.get_body(date:)
     unixtime = date.to_time.to_i
     uri = URI("https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user=kenzo1995&from_second=#{unixtime}")
     res = Net::HTTP.get_response(uri)
     res.body
+  end
+
+  def self.get_problems(date:)
+    prev_day = date
+    next_day = date + 1
+    result = JSON.parse(get_body(date: prev_day)) - JSON.parse(get_body(date: next_day))
+    result = result.select { |x| x["result"] = "WA" }
+    result.uniq { [_1["contest_id"], _1["problem_id"]] }
   end
 
   def self.yesterday_report
