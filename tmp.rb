af924f47a482e5b15dbc3ca848f4fb2d85c0d302
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-02 17:48:57 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -8,7 +8,6 @@ def ans(n:, k:, c:)
   kind = 0
   max = 1
 
-  # check kind, max
   n.times do |i|
     kind += 1 if hash[c[i].to_s] == 0
     hash[c[i].to_s] += 1
@@ -18,7 +17,7 @@ def ans(n:, k:, c:)
       kind -= 1 if hash[c[i-k].to_s] == 0
     end
 
-    max = [kind, max].max
+    max = [max, kind].max
   end
 
   max
@@ -27,24 +26,25 @@ end
 def main
   n,k = gets.chomp.split(' ').map(&:to_i)
   c = gets.chomp.split(' ').map(&:to_i)
-
-  print ans(n: n, k: k, c: c)
+  print ans(n:n,k:k,c:c)
 end
 
 class SampleTest < Minitest::Test
   def test_1
     expected = 3
-    actual = ans(n: 7, k: 3, c: %w[1 2 1 2 3 3 1])
+    actual = ans(n: 7, k: 3, c: [1,2,1,2,3,3,1])
     assert_equal expected, actual
   end
-  def test_3
-    expected = 1
-    actual = ans(n: 5, k: 5, c: %w[4 4 4 4 4])
-    assert_equal expected, actual
-  end
+
   def test_2
+    expected = 1
+    actual = ans(n: 5,k:5, c:[4,4,4,4,4])
+    assert_equal expected, actual
+  end
+
+  def test_3
     expected = 4
-    actual = ans(n: 10, k: 6, c: %w[304621362 506696497 304621362 506696497 834022578 304621362 414720753 304621362 304621362 414720753])
+    actual = ans(n: 10,k:6, c: %w[304621362 506696497 304621362 506696497 834022578 304621362 414720753 304621362 304621362 414720753])
     assert_equal expected, actual
   end
 end
