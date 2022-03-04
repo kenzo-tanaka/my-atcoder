493f1f1033ea2e239f02f7ec7c45f7a2105ecb02
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-04 18:12:27 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,50 +1,39 @@
 require 'minitest/autorun'
 
-def ans(n:, k:, c:)
-  # build hash
-  hash = Hash.new
-  c.each { |x| hash[x.to_s] = 0 }
-
-  kind = 0
-  max = 1
-
-  n.times do |i|
-    kind += 1 if hash[c[i].to_s] == 0
-    hash[c[i].to_s] += 1
-
-    if (i >= k)
-      hash[c[i-k].to_s] -= 1
-      kind -= 1 if hash[c[i-k].to_s] == 0
+def ans(s:, t:)
+  time = s+1
+  result = 0
+  time.times do |a|
+    (time - a).times do |b|
+      (time - a - b).times do |c|
+        result += 1 if a+b+c <= s && a*b*c <=t
+      end
     end
-
-    max = [max, kind].max
   end
-
-  max
+  result
 end
 
 def main
-  n,k = gets.chomp.split(' ').map(&:to_i)
-  c = gets.chomp.split(' ').map(&:to_i)
-  print ans(n:n,k:k,c:c)
+  s,t = gets.chomp.split(' ').map(&:to_i)
+  print ans(s:s,t:t)
 end
+
+main
 
 class SampleTest < Minitest::Test
   def test_1
-    expected = 3
-    actual = ans(n: 7, k: 3, c: [1,2,1,2,3,3,1])
-    assert_equal expected, actual
-  end
-
-  def test_2
-    expected = 1
-    actual = ans(n: 5,k:5, c:[4,4,4,4,4])
-    assert_equal expected, actual
-  end
-
-  def test_3
     expected = 4
-    actual = ans(n: 10,k:6, c: %w[304621362 506696497 304621362 506696497 834022578 304621362 414720753 304621362 304621362 414720753])
+    actual = ans(s: 1, t: 0)
+    assert_equal expected, actual
+  end
+  def test_2
+    expected = 213
+    actual = ans(s: 10, t: 10)
+    assert_equal expected, actual
+  end
+  def test_3
+    expected = 2471
+    actual = ans(s: 30, t: 100)
     assert_equal expected, actual
   end
 end
