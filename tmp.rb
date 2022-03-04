9b1d1158a6b58e3643f3c0c1e6026089643c75db
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-04 18:17:40 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,39 +1,11 @@
-require 'minitest/autorun'
-
-def ans(s:, t:)
-  time = s+1
-  result = 0
-  time.times do |a|
-    (time - a).times do |b|
-      (time - a - b).times do |c|
-        result += 1 if a+b+c <= s && a*b*c <=t
-      end
-    end
-  end
-  result
+def ans(s:)
+  %w[H 2B 3B HR].all? { |x| s.include? x } ? "Yes" : "No"
 end
 
 def main
-  s,t = gets.chomp.split(' ').map(&:to_i)
-  print ans(s:s,t:t)
+  s = []
+  4.times { s << gets.chomp }
+  print ans(s: s)
 end
 
 main
-
-class SampleTest < Minitest::Test
-  def test_1
-    expected = 4
-    actual = ans(s: 1, t: 0)
-    assert_equal expected, actual
-  end
-  def test_2
-    expected = 213
-    actual = ans(s: 10, t: 10)
-    assert_equal expected, actual
-  end
-  def test_3
-    expected = 2471
-    actual = ans(s: 30, t: 100)
-    assert_equal expected, actual
-  end
-end
