c6841655e3b7c4062e5907ea6270ef68aa43e83e
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-05 19:14:26 +0900

    tweak

--- code/alds1_1_a.rb
+++ code/alds1_1_a.rb
@@ -2,15 +2,17 @@ def ans(n, a)
   # 問題の仕様上初期表示
   puts a.join(' ')
   (1..(n-1)).each do |i|
-    value = a[i]
-    j = i - 1
+    # i = 1
+    value = a[i] # value = 2
+    j = i - 1 # j = 0
 
     # 1つ前の値a[j]がvalueよりも大きいとき
-    while j >= 0 && a[j] > value
-      a[j+1] = a[j]
-      j -= 1
+    while j >= 0 && a[j] > value # yes
+      a[j+1] = a[j] # a[1] = a[0]
+      j -= 1 # j = -1
     end
     # TODO: これが何かわかていない
+    # a[0] = 2
     a[j+1] = value
 
     puts a.join(' ')
