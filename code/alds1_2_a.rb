c96be475f98d38a0fe35f4ebb1aaf7296f4e5824
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-05 20:30:01 +0900

    tweak

--- code/alds1_2_a.rb
+++ code/alds1_2_a.rb
@@ -1,30 +1,47 @@
-def answer(array)
-    n = array.length
-    tmp = array.dup
-    counter = 0
+def answer(a, n)
+  [[1,2,3,4,5], 8]
 
-    for i in (0..(n-1))
-        j = n - 1
-        while j >= i+1
-            if tmp[j] < tmp[j-1]
-                pre = tmp[j-1]
-                nex = tmp[j]
-                tmp[j-1] = nex
-                tmp[j] = pre
-                counter += 1
-            end
-            j -= 1
-        end
+  flag = true
+
+  # ソート済みのindex
+  i = 0
+
+  # swap回数
+  swap = 0
+
+  while flag
+    flag = false
+
+    # jをn-1から小さくしていく
+    ((n-1).downto(i+1)).each do |j|
+      # 順番が逆であればswapする
+      if a[j] < a[j-1]
+        nex = a[j]
+        pre = a[j-1]
+
+        # swap
+        a[j] = pre
+        a[j-1] = nex
+
+        swap += 1
+
+        flag = true
+      end
     end
-    [tmp, counter]
+    i += 1
+  end
+
+  [a, swap]
 end
 
 def main
-    n = gets.chomp.to_i
-    a = gets.chomp.split(' ').map(&:to_i)
-    answer(a)
+  n = gets.chomp.to_i
+  a = gets.chomp.split(' ').map(&:to_i)
+
+  result = answer(a,n)
+
+  puts result[0].join(' ')
+  puts result[1]
 end
 
-array, counter = main
-puts array.join(' ')
-puts counter
+main
--- code/test/alds1_2_a_test.rb
+++ code/test/alds1_2_a_test.rb
@@ -4,13 +4,13 @@ require_relative '../alds1_2_a'
 class SampleTest < Minitest::Test
     def test_1
         expected = [[1,2,3,4,5], 8]
-        actual = answer([5,3,2,4,1])
+        actual = answer([5,3,2,4,1], 5)
         assert_equal expected, actual
     end
 
-    # def test_2
-    #     expected = true
-    #     actual = true
-    #     assert_equal expected, actual
-    # end
+    def test_2
+        expected = [[1,2,3,4,5,6], 9]
+        actual = answer([5,2,4,6,1,3], 6)
+        assert_equal expected, actual
+    end
 end
