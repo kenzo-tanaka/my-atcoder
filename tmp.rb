6ba22b5e60e0974ac135a0504007083ef4db0a27
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-07 20:29:12 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,53 +1,29 @@
-require 'minitest/autorun'
+def ans(n,k,c)
+  hash = {}
+  c.each { |x| hash[x] = 0 }
 
-def ans(n,k,a)
-  result = Array.new(n, default = k / n)
-  remain = k % n
+  kind = 0
+  max = 0
 
-  tmp = []
-  a.each_with_index { |x, idx| tmp << [x, idx] }
-  tmp.sort!
+  n.times do |i|
+    kind += 1 if hash[c[i]] == 0
+    hash[c[i]] += 1
 
-  remain.times do |i|
-    idx = tmp[i][1]
-    result[idx] += 1
+    if i >= k
+      hash[c[i-k]] -= 1
+      kind -= 1 if hash[c[i-k]] == 0
+    end
+
+    max = [max, kind].max
   end
 
-  result
+  max
 end
 
 def main
   n, k = gets.chomp.split(' ').map(&:to_i)
-  a = gets.chomp.split(' ').map(&:to_i)
-  puts ans(n,k,a)
+  c = gets.chomp.split(' ').map(&:to_i)
+  print ans(n,k,c)
 end
 
 main
-
-# class SampleTest < Minitest::Test
-#   def test_1
-#     expected = [4,3]
-#     actual = ans(2,7,[1,8])
-#     assert_equal expected, actual
-#   end
-#
-#   def test_2
-#     expected = [3]
-#     actual = ans(1,3,[33])
-#     assert_equal expected, actual
-#   end
-#
-#   def test_3
-#     expected = %w[
-#       142857142857
-#       142857142857
-#       142857142858
-#       142857142857
-#       142857142857
-#       142857142857
-#       142857142857
-#     ].map(&:to_i)
-#     actual = ans(7,1000000000000,[99, 8, 2, 4, 43, 5, 3])
-#     assert_equal expected, actual
-#   end
-# end
