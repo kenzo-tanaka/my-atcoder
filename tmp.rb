7b59b6e38cbf835a00763494613f9e480d9878b7
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-07 20:16:55 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,42 +1,53 @@
-def ans(n,as)
-  ls = []
-  rs = []
+require 'minitest/autorun'
 
-  as.each do |a|
-    t = a[0]
-    l = a[1]
-    r = a[2]
+def ans(n,k,a)
+  result = Array.new(n, default = k / n)
+  remain = k % n
 
-    l *= 2
-    r *= 2
+  tmp = []
+  a.each_with_index { |x, idx| tmp << [x, idx] }
+  tmp.sort!
 
-    if t == 3 || t == 4
-      l += 1
-    end
-
-    if t ==2 || t == 4
-      r -= 1
-    end
-
-    ls << l
-    rs << r
+  remain.times do |i|
+    idx = tmp[i][1]
+    result[idx] += 1
   end
 
-  cnt = 0
-  (0..(n-1)).each do |i|
-    (i+1..(n-1)).each do |j|
-      cnt += 1 if [ls[i], ls[j]].max <= [rs[i], rs[j]].min
-    end
-  end
-  cnt
+  result
 end
 
 def main
-  n = gets.chomp.to_i
-  a = []
-  n.times { a<< gets.chomp.split(' ').map(&:to_i) }
-
-  print ans(n,a)
+  n, k = gets.chomp.split(' ').map(&:to_i)
+  a = gets.chomp.split(' ').map(&:to_i)
+  puts ans(n,k,a)
 end
 
 main
+
+# class SampleTest < Minitest::Test
+#   def test_1
+#     expected = [4,3]
+#     actual = ans(2,7,[1,8])
+#     assert_equal expected, actual
+#   end
+#
+#   def test_2
+#     expected = [3]
+#     actual = ans(1,3,[33])
+#     assert_equal expected, actual
+#   end
+#
+#   def test_3
+#     expected = %w[
+#       142857142857
+#       142857142857
+#       142857142858
+#       142857142857
+#       142857142857
+#       142857142857
+#       142857142857
+#     ].map(&:to_i)
+#     actual = ans(7,1000000000000,[99, 8, 2, 4, 43, 5, 3])
+#     assert_equal expected, actual
+#   end
+# end
