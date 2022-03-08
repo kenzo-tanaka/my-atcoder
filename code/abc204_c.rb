c2abe99e03705957f01b91c2b668b00ae413decb
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-08 19:57:35 +0900

    tweak

--- code/abc204_c.rb
+++ code/abc204_c.rb
@@ -1,35 +1,10 @@
 def ans(n,m,abs)
-  a_array = []
-  b_array = []
-
-  routes = []
-
-  # (1,1)などがあるので、Nを初期値とする
-  result = n
-  m.times do |i|
-    ab = abs[i]
+  graph = Array.new(n, default = [])
+  # 直接繋がっている都市リスト作成
+  abs.each do |ab|
     a,b = ab[0], ab[1]
-    a_array << a
-    b_array << b
-
-    unless routes.include? ab
-      routes << ab
-      result += 1
-    end
-
-    if b_array.include? a
-      idx = b_array.index(a)
-      start_point = a_array[idx]
-      end_point = b
-
-      unless start_point != end_point
-        routes << [start_point, end_point]
-        result += 1
-      end
-    end
+    graph[a-1] << b-1
   end
 
-  print routes
-
-  result
+  7
 end
