edfb87f6d44384a553f95096d4865b9ca3739d26
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-08 19:02:44 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,36 +1,13 @@
-def ans(h,w,n,abs)
-  as, bs = [], []
-  abs.each_with_index { |x, idx| as[idx], bs[idx] = x[0], x[1] }
+def ans(a,b,c,x)
+  return 1.0 if x <= a
+  return 0 if x > b
 
-  a_hash, b_hash = {}, {}
-  as.sort.uniq.each_with_index do |x, idx|
-    a_hash[x] = idx + 1
-  end
-  bs.sort.uniq.each_with_index do |x, idx|
-    b_hash[x] = idx + 1
-  end
-
-  result = []
-
-  abs.each do |ab|
-    a = ab[0]
-    b = ab[1]
-
-    result << [a_hash[a], b_hash[b]]
-  end
-
-  result
+  c.to_f / (b - a).to_f
 end
 
 def main
-  h,w,n = gets.chomp.split(' ').map(&:to_i)
-  abs = []
-  n.times { abs << gets.chomp.split(' ').map(&:to_i) }
-
-  result = ans(h,w,n, abs)
-  result.each do |x|
-    puts x.join(" ")
-  end
+  a,b,c,x = gets.chomp.split(' ').map(&:to_i)
+  print ans(a,b,c,x)
 end
 
 main
