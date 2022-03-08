95120fde4e8df8b896d0efc2248c553f5c59d31c
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-08 20:13:41 +0900

    tweak

--- code/abc204_c.rb
+++ code/abc204_c.rb
@@ -1,10 +1,31 @@
+TMP=Array.new(2000, default = false)
+G = Array.new(2000, default = [])
+
+def dfs(v)
+  return TMP[v] if TMP[v]
+
+  TMP[v] = true
+  for vv in G[v]
+    dfs(vv)
+  end
+end
+
 def ans(n,m,abs)
-  graph = Array.new(n, default = [])
   # 直接繋がっている都市リスト作成
   abs.each do |ab|
     a,b = ab[0], ab[1]
-    graph[a-1] << b-1
+    G[a-1] << b-1
   end
 
-  7
+  result = 0
+  n.times do |i|
+    TMP.length.times do |i|
+      TMP[i] = false
+    end
+
+    dfs(i)
+    result += TMP.count { |x| x == true }
+  end
+
+  result
 end
