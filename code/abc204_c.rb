b2e080d459db5e2f161d78128b03d648a2aaf0f0
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-08 21:04:41 +0900

    tweak

--- code/abc204_c.rb
+++ code/abc204_c.rb
@@ -1,32 +1,40 @@
-TMP = Array.new(2000, default = false)
-G = Array.new(2000, default = [])
-
-def dfs(v)
-  return TMP[v] if TMP[v]
-
-  TMP[v] = true
-  G[v].each { |vv| dfs(vv) }
-end
-
-def tmp_reset
-  TMP.length.times { |i| TMP[i] = false }
-end
-
-def ans(n,m,abs)
-  # 直接繋がっている都市リスト作成
-  abs.each do |ab|
-    a,b = ab[0], ab[1]
-    G[a-1] << b-1
+# TMP = Array.new(2000, default = false)
+class Dfs
+  def initialize(n,m,abs)
+    @n = n
+    @m = m
+    @abs = abs
+    @graph = Array.new(n, default = [])
+    @tmp = Array.new(n, default = false)
   end
 
-  result = 0
-  n.times do |i|
-    tmp_reset
-    dfs(i)
-    result += TMP.count { |x| x }
+  def tmp_reset
+    @n.times { |i| @tmp[i] = false }
   end
 
-  result
+  def dfs(v)
+    return @tmp[v] if @tmp[v]
+
+    @tmp[v] = true
+    @graph[v].each { |vv| dfs(vv) }
+  end
+
+  def build_graph
+    # 直接繋がっている都市リスト作成
+    @abs.each { |ab| @graph[ab[0]-1] << ab[1]-1 }
+  end
+
+  def ans
+    build_graph
+    result = 0
+    @n.times do |i|
+      tmp_reset
+      dfs(i)
+      result += @tmp.count { |x| x }
+    end
+
+    result
+  end
 end
 
 # def main
@@ -34,7 +42,7 @@ end
 #   abs = []
 #   m.times { abs << gets.chomp.split(' ').map(&:to_i) }
 #
-#   print ans(n,m,abs)
+#   print Dfs.new(n,m,abs).ans
 # end
 #
 # main
--- code/test/abc204_c_test.rb
+++ code/test/abc204_c_test.rb
@@ -4,19 +4,22 @@ require_relative '../abc204_c'
 class SampleTest < Minitest::Test
   def test_1
     expected = 7
-    actual = ans(3,3,[[1,2], [2,3], [3,2]])
+    instance = Dfs.new(3,3,[[1,2], [2,3], [3,2]])
+    actual = instance.ans
     assert_equal expected, actual
   end
 
   def test_2
     expected = 3
-    actual = ans(3,0,[])
+    instance = Dfs.new(3,0,[])
+    actual = instance.ans
     assert_equal expected, actual
   end
 
   def test_3
     expected = 16
-    actual = ans(4,4,[[1,2], [2,3], [3,4], [4,1]])
+    instance = Dfs.new(4,4,[[1,2], [2,3], [3,4], [4,1]])
+    actual = instance.ans
     assert_equal expected, actual
   end
 end
