3ff2176a64d276a22ae4f58dfd3b29088bab3758
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-13 21:28:37 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,14 +1,11 @@
-def ans(n,k,a)
-  result = Array.new(n, default = k/n)
-  k %= n
+def ans(n,k,abs)
+  abs.sort!
+  result = k
+  j = 0
 
-  tmp = []
-  a.each_with_index { |x, idx| tmp << [x, idx] }
-  tmp.sort!
-
-  k.times do |i|
-    # i番目に大きいaの値のindex
-    result[tmp[i][1]] += 1
+  while j < n && abs[j][0] <= result
+    result += abs[j][1]
+    j += 1
   end
 
   result
@@ -16,9 +13,27 @@ end
 
 def main
   n,k = gets.chomp.split(' ').map(&:to_i)
-  a = gets.chomp.split(' ').map(&:to_i)
+  abs = []
+  n.times { abs << gets.chomp.split(' ').map(&:to_i) }
 
-  puts ans n,k,a
+  puts ans n,k,abs
 end
 
 main
+
+#
+# require 'minitest/autorun'
+#
+# class SampleTest < Minitest::Test
+#   def test_1
+#     expected = 4
+#     actual = ans(2,3, [[2,1], [5,10]])
+#     assert_equal expected, actual
+#   end
+#
+#   def test_2
+#     expected = true
+#     actual = true
+#     assert_equal expected, actual
+#   end
+# end
