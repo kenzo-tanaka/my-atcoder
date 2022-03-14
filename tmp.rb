4c1b71fbdc0dd02ffd889b982ce589334891c016
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-14 22:04:45 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,25 +1,28 @@
-def ans(n)
-  # Set default values
-  dp = Array.new(11, default = 0)
-  9.times { |i| dp[i+1] = 1 }
-  mod = 998244353
+def ans(h,w,n,abs)
+  a,b = [], []
+  abs.each_with_index { |ab,idx| a[idx], b[idx] = ab[0], ab[1] }
 
-  (n-1).times do
-    new_arr = Array.new(11, default = 0)
-    9.times do |j|
-      j += 1
-      new_arr[j] = (dp[j-1] + dp[j] + dp[j+1]) % mod
-    end
+  tmp_a, tmp_b = {}, {}
+  a.uniq.sort.each_with_index { |x,idx| tmp_a[x] = idx + 1 }
+  b.uniq.sort.each_with_index { |x,idx| tmp_b[x] = idx + 1 }
 
-    dp = new_arr
+  result = []
+  abs.each do |ab|
+    result << [ tmp_a[ab[0]], tmp_b[ab[1]] ]
   end
 
-  dp.sum % mod
+  result
 end
 
 def main
-  n = gets.chomp.to_i
-  print ans n
+  h,w,n = gets.chomp.split(' ').map(&:to_i)
+  abs = []
+  n.times { abs << gets.chomp.split(' ').map(&:to_i) }
+  result = ans(h,w,n,abs)
+
+  result.each do |x|
+    puts x.join ' '
+  end
 end
 
 main
