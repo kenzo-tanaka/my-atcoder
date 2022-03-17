a7a8b9ec69b73780c99b95bc1513cb5e9f157f9c
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-17 21:23:25 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,27 +1,15 @@
-h,w,n = gets.chomp.split(' ').map(&:to_i)
-a,b = [], []
+n, k = gets.chomp.split(' ').map(&:to_i)
 abs = []
-n.times do
-  ab = gets.chomp.split(' ').map(&:to_i)
-  abs << ab
-  a << ab[0]
-  b << ab[1]
+n.times { abs << gets.chomp.split(' ').map(&:to_i) }
+
+abs.sort!
+
+j = 0
+result = k
+
+while j < n && abs[j][0] <= result
+  result += abs[j][1]
+  j += 1
 end
 
-a.sort!
-b.sort!
-
-hash_a, hash_b = {}, {}
-
-a.uniq.each_with_index do |x, idx|
-  hash_a[x] = idx + 1
-end
-
-b.uniq.each_with_index do |x, idx|
-  hash_b[x] = idx + 1
-end
-
-abs.each do |ab|
-  puts "#{hash_a[ab[0]]} #{hash_b[ab[1]]}"
-end
-
+pp result
