a3414510d3159d0d41cd814ce5a6b07c81650723
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-20 22:22:11 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,38 +1,27 @@
 n = gets.chomp.to_i
-t = gets.chomp.split('')
+puts 1
+STDOUT.flush
 
+# 使用済み
+array = []
+array << 1
 
-def change_head(head)
-  return "南" if head == "東"
-  return "西" if head == "南"
-  return "北" if head == "西"
-
-  "東"
-end
-
-def change_xy(head, xy)
-  if head == "東"
-    xy[0] += 1
-  elsif head == "西"
-    xy[0] -= 1
-  elsif head == "北"
-    xy[1] += 1
-  elsif head == "南"
-    xy[1] -= 1
-  end
-
-  xy
-end
-
-head = "東"
-xy = [0,0]
-
-t.each do |tt|
-  if tt == "R"
-    head = change_head(head)
+flag = true
+while flag
+  res = gets.chomp.to_i
+  if res == 0
+    flag = false
   else
-    xy = change_xy(head, xy)
+    array << res
+
+    # TODO: perf
+    (1..(2 * n + 1)).each do |i|
+      next if array.include? i
+
+      array << i
+      puts i
+      STDOUT.flush
+      break
+    end
   end
 end
-
-puts "#{xy[0]} #{xy[1]}"
