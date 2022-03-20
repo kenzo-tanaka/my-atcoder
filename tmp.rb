39bbe60195f98127e0f7efb3c0451ded8d313dfe
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-20 10:09:12 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,15 +1,28 @@
-n, k = gets.chomp.split(' ').map(&:to_i)
-abs = []
-n.times { abs << gets.chomp.split(' ').map(&:to_i) }
+n = gets.chomp.to_i
+xy = []
+n.times { xy << gets.chomp.split(' ').map(&:to_i) }
+s = gets.chomp
+s.split('').each_with_index { |ss, idx| xy[idx] << ss }
 
-abs.sort!
+# y座標が同じものでgroup
+grouped = xy.group_by { |element| element[1] }
 
-j = 0
-result = k
+result = "No"
 
-while j < n && abs[j][0] <= result
-  result += abs[j][1]
-  j += 1
+grouped.each_value do |array|
+  # あらかじめソートしておくことで
+  # indexが隣り合うものをチェックするだけでよくなる
+  array.sort!
+  array.each_with_index do |el, idx|
+    next if idx == 0
+
+    if array[idx-1][2] == "R" && el[2] == "L"
+      result = "Yes"
+      break
+    end
+  end
 end
 
-pp result
+puts result
+
+
