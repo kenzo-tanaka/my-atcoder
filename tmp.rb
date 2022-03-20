a4c6d2265747cbf78caab4e8037fdd1477ac21c0
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-20 21:02:47 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,24 +1,4 @@
-n, m = gets.chomp.split(' ').map(&:to_i)
-a = gets.chomp.split(' ').map(&:to_i)
-b = gets.chomp.split(' ').map(&:to_i)
-used = Array.new(m, default = false)
+n = gets.chomp.to_i
+s = gets.chomp
 
-result = "Yes"
-b.each do |bb|
-  flag = false
-
-  a.each_with_index do |aa, idx|
-    if bb == aa && used[idx] != true
-      flag = true
-      used[idx] = true
-      break
-    end
-  end
-
-  if flag == false
-    result = "No"
-    break
-  end
-end
-
-puts result
+puts s.split('').last
