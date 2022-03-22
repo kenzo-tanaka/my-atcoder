0fe677e9915f129d972f63351f10dcdec070d534
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-22 20:33:22 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,16 +1,13 @@
 n = gets.chomp.to_i
 
-def doubled(n)
+def double(n)
   (n.to_s * 2).to_i
 end
 
-j,count = 1, 0
-
-while doubled(j) <= n
+j,count = 1,0
+while double(j) <= n
   count += 1
   j += 1
 end
 
 puts count
-
-
