d7f7c131f416eaa4eb6ac17d16547818d6946f05
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-22 21:31:34 +0900

    tweak

--- code/abc241_c.rb
+++ code/abc241_c.rb
@@ -2,7 +2,23 @@ n = gets.chomp.to_i
 s = []
 n.times { s << gets.chomp.split('') }
 
-def check(array)
-  array.each_with_index do |ss, idx|
+def check(array, time)
+  flag = false
+  time.times do |i|
+    tmp = array[i..i+5]
+    if tmp.count("#") >= 4
+      flag = true
+      break
+    end
   end
+
+  flag
 end
+
+flag = false
+s.each do |ss_array|
+  flag = check(ss_array, n - 5)
+  break if flag
+end
+
+puts flag ? "Yes" : "No"
