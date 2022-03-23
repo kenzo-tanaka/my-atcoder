11094115c3f21153e40d89fd3a1c620057948371
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-23 20:00:55 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,13 +1,15 @@
-n = gets.chomp.to_i
+n, k = gets.chomp.split(' ').map(&:to_i)
+ab = []
+n.times { ab << gets.chomp.split(' ').map(&:to_i) }
 
-def double(n)
-  (n.to_s * 2).to_i
-end
+ab.sort!
+j = 0
+result = k
 
-j,count = 1,0
-while double(j) <= n
-  count += 1
+while j < n && result >= ab[j][0]
+  result += ab[j][1]
   j += 1
 end
 
-puts count
+puts result
+
