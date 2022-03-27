086a9e30c59ebb1564f07b7197390387572e00b9
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-27 21:39:23 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,25 +1,12 @@
-n = gets.chomp.to_i
-used = Array.new(2*n + 1, default = false)
+s, t = gets.chomp.split(' ').map(&:to_i)
 
-# set used
-puts 1
-STDOUT.flush
-used[0] = true
-
-while true
-  i = gets.chomp.to_i
-  break if i == 0
-
-  used[i-1] = true
-  used.each_with_index do |e,idx|
-    if e
-      next
-    else
-      puts idx+1
-      STDOUT.flush
-
-      used[idx] = true
-      break
+result = 0
+(0..s).each do |a|
+  (0..(s-a)).each do |b|
+    (0..(s-a-b)).each do |c|
+      result += 1 if (a+b+c) <= s && (a*b*c) <= t
     end
   end
 end
+
+puts result
