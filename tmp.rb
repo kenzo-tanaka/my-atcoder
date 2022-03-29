4cec43ea3ba7cb379f69b8637decf6795a60ea3f
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-29 19:30:59 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,21 +1,6 @@
-h,w,n = gets.chomp.split(' ').map(&:to_i)
-ab = []
-n.times { ab << gets.chomp.split(' ').map(&:to_i) }
+s = []
+4.times { s << gets.chomp }
 
-a, b = [], []
-n.times { |i| a[i], b[i] = ab[i][0], ab[i][1] }
+result = ['H', '2B', '3B', 'HR'].all? { |v| s.include? v }
 
-
-def build_hash(array)
-  hash = {}
-  array.sort.uniq.each_with_index do |x, idx|
-    hash[x] = idx + 1
-  end
-
-  hash
-end
-
-a_hash, b_hash = build_hash(a), build_hash(b)
-n.times do |i|
-  puts "#{a_hash[a[i]]} #{b_hash[b[i]]}"
-end
+puts result ? "Yes" : "No"
