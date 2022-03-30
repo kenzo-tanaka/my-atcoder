bd5489b2db70281e5ca97c4443d302616dced3c4
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-03-30 20:40:13 +0900

    tweak

---
+++ code/abc245_c.rb
@@ -0,0 +1,4 @@
+n, k = gets.chomp.split(' ').map(&:to_i)
+a = gets.chomp.split(' ').map(&:to_i)
+b = gets.chomp.split(' ').map(&:to_i)
+
--- code/test/abc245_c_test.rb
+++ code/test/abc245_c_test.rb
@@ -13,18 +13,3 @@ class SampleTest < Minitest::Test
     assert_equal expected, actual
   end
 end
-require 'minitest/autorun'
-
-class SampleTest < Minitest::Test
-  def test_1
-    expected = true
-    actual = true
-    assert_equal expected, actual
-  end
-
-  def test_2
-    expected = true
-    actual = true
-    assert_equal expected, actual
-  end
-end
