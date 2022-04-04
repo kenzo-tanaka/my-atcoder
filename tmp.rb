bccf737ee534d00e4920475d200f50a63c29a0e7
kenzo-tanaka <kenzou.kenzou104809@gmail.com>
2022-04-04 20:33:45 +0900

    tweak

--- tmp.rb
+++ tmp.rb
@@ -1,8 +1,28 @@
-a = [100,100,200]
+n = gets.chomp.to_i
 
-17.times do |i|
-  x = a[i] + a[i+1] + a[i+2]
-  a << x
+used = Array.new(2 * n + 1, default = false)
+puts 1
+STDOUT.flush
+
+used[0] = true
+
+while true
+  x = gets.chomp.to_i
+  if x == 0
+    break
+  else
+    # used
+    used[x-1] = true
+
+    # output
+    used.each_with_index do |e, idx|
+      next if e == true
+
+      used[idx] = true
+      puts idx + 1
+      STDOUT.flush
+
+      break
+    end
+  end
 end
-
-p a.last
